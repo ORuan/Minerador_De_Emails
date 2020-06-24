f58bbc5d0eddda787bf4a74687f718815d01a22b
ORuan <ruanpablomp@gmail.com>
2020-06-24 04:13:34 -0300

    algoritmo de expressaão regular alterado; bibliotecas para requests novas; readme explicativo

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-## Aprendendo a trabalhar com extração de dados
+# Aprendendo a trabalhar com extração de dados
+### WEB Crawling
+
+### Objetivos
+Trabalhar com requisções web atrás de email relacionados ai site em questão,
+são retornados de 90 a 100% de todos emails existentes nas paginas.
+---
+Indicado para Pentesters
+
+### Como usar
+```
+Antes de tudo, clone o repositório, e crie uma virtualenv, 
+que será onde ele será executado. Após isso, execute o comando
+pip install -r requeriments.txt, para instalar todas dependências
+do projeto.
+```
+---
+```
+$python3 inity.py
+
+A seguir você deve digitar o link do site em questão, 
+a versão 1 do projeto não suporta http, apenas https
+
+!É recomendando que se use a página HOME do site como url!
+
+
+```
+## O que isso retorna?
+### Os email encontrados serão armazenados em um arquivo de texto chamado emails.txt
+
--- inity.py
+++ inity.py
@@ -5,21 +5,23 @@ from bs4 import BeautifulSoup
 def getUrl():
     return input('Url do site [com http ou https)]:')
 
-#https://api.github.com/users/ORuan
+
 def request():
     
         url = getUrl()
         #r = requests.get(url)
         html_page = urllib.request.urlopen(url)
-
-
         soup = BeautifulSoup(html_page, features="html.parser")
-        strsoup = str(soup)
-        achados = re.findall('.........@......................', strsoup)
-        email_file = io.open('./emails.txt', 'w')
-        email_file.write(' '+ str(achados))
-        email_file.close()
 
+        try :
+            request_page = requests.get(url)
+            response_text = soup.text
+            encontrados = re.findall('\w+@\w*\.\w+', response_text)
+            email_file = io.open('./emails.txt', 'w')
+            email_file.write('-------'+ str(encontrados))
+            email_file.close()
+        except:
+            print('nao encontramos email, mb')
 
         for link in soup.findAll('a'):
             
@@ -53,40 +55,33 @@ def request():
                 except:
                     print('Problema interno')
             
-            
-            
         
 def new_request(link,url_old):
     try:
 
-        new_html_page = urllib.request.urlopen(link)
-        newsoup = BeautifulSoup(new_html_page, features="html.parser")
-        strnewsoup = str(newsoup)
-        achados = re.findall('.........@......................', strnewsoup)
-        for ac in achados:
+        
+            request_page = requests.get(url)
+            response_text = soup.text
+            encontrados = re.findall('\w+@\w*\.\w+', response_text)
             email_file = io.open('./emails.txt', 'a')
-            email_file.write(' '+ str(ac))
+            email_file.write('-------'+ str(encontrados))
             email_file.close()
+
 
     except:
         
         complet_link = url_old + link
-        new_html_page = urllib.request.urlopen(link)
-        newsoup_ = BeautifulSoup(new_html_page, features="html.parser")
-        strnewsoup_ = str(newsoup_)
-        achados_ = re.findall('.........@......................', strnewsoup_)
-        for ac_ in achados_:
-            email_file = io.open('./emails.txt', 'a')
-            email_file.write(' '+ str(ac_))
-            email_file.close()
+                
+        request_page = requests.get(complet_link)
+        response_text = soup.text
+        encontrados = re.findall('\w+@\w*\.\w+', response_text)
+        email_file = io.open('./emails.txt', 'a')
+        email_file.write('-------'+ str(encontrados))
+        email_file.close()
             
 
 
     else:
         print('nao conseguimos nos conectar, srry')
-
-
-#def request_inside(link):
-
 
 request()
